2ccb2ae53205713d8cc7abddcdd04ab61691efad
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-10-15 16:57:09 -0400

    added a generateREADME const

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const inquirer = require('inquirer')
 console.log(inquirer)
 const fs = require('fs')
-console.log(fs)
+console.log(fs) 
 
+const generateREADME = ({title, description, installation, usage, contribution, test, questions})
+console.log(generateREADME)
 // TODO: Create an array of questions for user input
-// Needed questions, 1. What is my project title?, 2. The description, 3. Installation instructions, 4. Usage, 5. Contribution guidelines, 6. Test instructions 
+// Needed questions, 1. What is my project title?, 2. The description, 3. Installation instructions, 4. Usage, 5. Contribution guidelines, 6. Test instructions, 7. Questions
 const questions = ([
     {
         type: 'input',
@@ -41,6 +43,12 @@ const questions = ([
         type: 'input',
         name: 'Test instructions',
         message: 'How do you test this application'
+    },
+
+    {
+        type: 'input',
+        name: 'Questions to address',
+        message: 'Are there any questions you would like to address for the user'
     }
 ]);
 
@@ -48,8 +56,7 @@ console.log(questions)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    var fileName = README
-    console.log(fileName)
+    
 }
 
 // TODO: Create a function to initialize app
